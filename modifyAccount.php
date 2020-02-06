049ff02132ef4ab6a5bffe08379efe1a9b0775e8
Antoine Marc <amarc@student.42.fr>
2020-02-06 11:49:40 +0100

    modifiy account view css

--- modifyAccount.php
+++ modifyAccount.php
@@ -4,6 +4,9 @@ session_start();
 require('model/generalModel.php');
 include ('config/database.php');
 
+if(empty($_SESSION['login']))
+    header('Location: login.php');
+
 $profile = get_profile($_SESSION['login']);
 
 require('./view/modifyAccountView.php');
--- view/modifyAccountView.php
+++ view/modifyAccountView.php
@@ -7,21 +7,25 @@
 <section id="content">
     <div class="modify-account">
         <div class="modify-form">
-            <h3>Modifier vos informations</h3>
+            <h3>Modifier le profil</h3>
             <p style="font-weight:bold; color: #DA2C38"><?= $error ?></p>
             <form class="subscription-form" action="" method="post">
                 <table>
                     <tr>
                         <td><p>Nom d'utilisateur</p></td>
-                        <td><input type="text" name="login" value=<?= $profile['login']; ?> required maxlength="12"></td>
+                        <td><input type="text" name="login" value="<?= $profile['login']; ?>" required maxlength="12"></td>
                     </tr>
                     <tr>
                             <td><p>Email</p></td>
-                            <td><input type="email" name="mail" value=<?= $profile['mail']; ?> required maxlength="40"></td>
+                            <td><input type="email" name="mail" value="<?= $profile['mail']; ?>" required maxlength="40"></td>
                     </tr>
                     <tr>
                         <td><p>Nom complet</p></td>
-                        <td><input type="text" name="name" value=<?= $profile['name']; ?> required maxlength="15"></td>
+                        <td><input type="text" name="name" value="<?= $profile['name']; ?>" required maxlength="15"></td>
+                    </tr>
+                    <tr>
+                        <td><p>Bio</p></td>
+                        <td><textarea type="text" name="bio"><?= $profile['bio']; ?></textarea></td>
                     </tr>
                     <tr>
                         <td></td>
@@ -31,26 +35,26 @@
             </form>
         </div>
         <br>
-        <div class="modify-form">
-            <h3>Modifier vos informations</h3>
+        <div class="modify-form password-form">
+            <h3>Changer de mot de passe</h3>
             <p style="font-weight:bold; color: #DA2C38"><?= $error ?></p>
             <form class="subscription-form" action="" method="post">
                 <table>
                     <tr>
-                        <td><p>Nom d'utilisateur</p></td>
-                        <td><input type="text" name="login" value=<?= $profile['login']; ?> required maxlength="12"></td>
+                        <td><p>Ancien mot de passe</p></td>
+                        <td><input type="password" name="oldPasswd" required></td>
                     </tr>
                     <tr>
-                            <td><p>Email</p></td>
-                            <td><input type="email" name="mail" value=<?= $profile['mail']; ?> required maxlength="40"></td>
+                        <td><p>Nouveau mot de passe</p></td>
+                        <td><input type="password" name="newPasswd1" required></td>
                     </tr>
                     <tr>
-                        <td><p>Nom complet</p></td>
-                        <td><input type="text" name="name" value=<?= $profile['name']; ?> required maxlength="15"></td>
+                        <td><p>Confirmer le nouveau mot de passe</p></td>
+                        <td><input type="password" name="newPasswd2" required></td>
                     </tr>
                     <tr>
                         <td></td>
-                        <td><input type="submit" name="submit" value="Sauvegarder"></td>
+                        <td><input type="submit" name="submit" value="Modifier le mot de passe"></td>
                     </tr>
                 </table>
             </form>
