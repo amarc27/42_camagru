b4007e555184e9d0a18f866c446a7fe035ac7f2d
Antoine Marc <amarc@student.42.fr>
2020-02-13 19:36:59 +0100

    created picture model

--- model/generalModel.php
+++ model/generalModel.php
@@ -1,6 +1,6 @@
 <?php
-
 include ('userModel.php');
+include ('picturesModel.php');
 
 function db_connect() 
 {
@@ -86,29 +86,4 @@ function password_secure($password)
 function redir($redir_path)
 {
     echo "<script>setTimeout(\"location.href = \'$redir_path\';\",2000);</script>";
-}
-
-
-/* GALLERY */
-
-function add_picture($login, $file) {
-    $profile = get_profile($login);
-    $db = db_connect();
-
-    $sql = "INSERT INTO picture (id_user, img) VALUES ('".$profile['id']."', '".$file."')";
-    $db->query($sql);
-    $db = NULL;
-    return true;
-}
-
-function get_pics($login) {
-	$profile = get_profile($login);
-	$db = db_connect();
-
-	// $sql = $db->prepare("SELECT * FROM picture WHERE id_user = :id");
-	//$sql->bindParam(":id", $profile['id'], PDO::PARAM_INT);
-
-	$sql = 'SELECT * FROM picture WHERE id_user = "'.$profile['id'].'"';
-	$data = $db->query($sql);
-	return ($data);
 }
