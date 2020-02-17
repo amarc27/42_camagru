ea9ecb3085a7887dc75a5cf66240c6fe5feb85b8
Antoine Marc <amarc@student.42.fr>
2020-02-17 11:54:33 +0100

    css on signup and login

--- view/loginView.php
+++ view/loginView.php
@@ -5,7 +5,7 @@
 <section id="content">
     <div class="log-page">
         <h1>Camagru</h1>
-        <h3>Sign up to see photos and videos from your friends</h3>
+        <h3>Log in to see photos and videos from your friends</h3>
         <p style="font-weight:bold; color: #DA2C38"><?= $error ?></p>
         <form class="subscription-form" action="" method="post">
             <input type="text" name="login" placeholder="Nom d'utilisateur" required maxlength="12">
